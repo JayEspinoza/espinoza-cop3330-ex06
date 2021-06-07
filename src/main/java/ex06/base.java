c88d7e21cafd4d331d4e32897608f1d2574d463c
Julian <julianespinoza@knights.ucf.edu>
2021-06-06 21:01:26 -0400

    Initial Solution

--- src/main/java/ex06/base.java
+++ src/main/java/ex06/base.java
@@ -2,7 +2,7 @@
  *  UCF COP3330 Summer 2021 Assignment 6 Solution
  *  Copyright 2021 Julian Espinoza
  *
- *  Exercise 6 - Mad Lib
+ *  Exercise 6 - Retirement Calculator
  *  Create a program that determines how many years you have left until retirement and the
  *  year you can retire. It should prompt for your current age and the age you want to retire and display
  *  the output as shown in the example that follows.
